46524f672b56fdf3e9db50221b88117f1144bbb5
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2020-11-26 19:57:41 +0530

    Update en.py

--- bot/locals/en.py
+++ bot/locals/en.py
@@ -2,8 +2,8 @@
 from bot.locals import Local
 
 LOCAL = Local({
-    'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{CHAT_ID}</code>',
-    'WELCOME_MESSAGE' : "Hi!\nI'm <b>Botkaca</b>!\nPowered by pyrogram.\nFork: <a href='t.me/touka19</a>",
+    'WRONG_ROOM' : 'I AM NOT MADE FOR THIS GROUP.\nID : <code>{CHAT_ID}</code>',
+    'WELCOME_MESSAGE' : "YOU DONT HAB PERMISSTION TO USE ME</a>",
     'PASS_REQUIRED' : '\n\nUse <code>/{cmd_pass} </code>to enter the password.',
     'LEECH_LIST_MESSAGE_HEADER' : '<b>Leech Status</b>',
     'LEECH_LIST_FORMAT' : 'Name: <code>{name}</code>\nStatus: {status}\nID: <code>{gid}</code>\n\n',
@@ -42,6 +42,6 @@ LOCAL = Local({
     'COMMAND_UPLOAD_AS_DOC' : 'toggle upload anything as document',
     'COMMAND_UPLOAD_AS_ZIP' : 'toggle upload anything as bundled zipfile',
     'COMMAND_SET_TRACKER' : 'set default tracker, sparated by newline',
-    'BLOCK_EMPTY' : "▫️",
-    "BLOCK_FILLED" : "◾️"
+    'BLOCK_EMPTY' : "❤️",
+    "BLOCK_FILLED" : " 🖤 "
 })
